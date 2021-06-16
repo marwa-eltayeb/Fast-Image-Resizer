7bfe9049b9be789b5cd841967a8d2ba71b9c22f9
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-06-16 17:53:56 +0200

    Resize height of the main window

--- src/com/marwaeltayeb/fir/Main.java
+++ src/com/marwaeltayeb/fir/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui/main.fxml"));
         primaryStage.setTitle("Fast Image Resizer");
-        primaryStage.setScene(new Scene(root, 650, 400));
+        primaryStage.setScene(new Scene(root, 650, 385));
         primaryStage.getIcons().add(new Image("/com/marwaeltayeb/fir/assets/logo.png"));
         primaryStage.getScene().getStylesheets().add(getClass().getResource("stylesheet/application.css").toExternalForm());
         primaryStage.setResizable(false);
